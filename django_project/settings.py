4e8c799346b733bc8f64c47115228cdb3b7ff941
Kyiro <josereyesiriarte@gmail.com>
2025-02-22 17:21:18 -0400

    cors headers

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'posts',
+    "corsheaders",
 ]
 
 REST_FRAMEWORK = {
@@ -51,11 +52,21 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [    #Aqui se especifica la URL de la aplicación cliente, que puede ser un dominio o un IP (lo hizo la ia :))
+    "http://localhost:3000", 
+    "http://localhost:8000",
+]
+
+CORS_TRUSTED_ORIGINS = [  
+    "http://localhost:3000", 
 ]
 
 ROOT_URLCONF = 'django_project.urls'
