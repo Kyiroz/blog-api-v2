eb967c63e36095db4f0701aca94b990e67e016ba
Kyiro <josereyesiriarte@gmail.com>
2025-02-22 16:30:51 -0400

    list post and detail done

---
+++ posts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date_posted', 'updated')
+    list_filter = ('author', 'date_posted')
+    search_fields = ('title', 'content', 'author__username') #Campos de busqueda por:
+    ordering = ('-date_posted',)
+
+admin.site.register(Post, PostAdmin)
