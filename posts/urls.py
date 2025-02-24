f499ada0f6b58d4e8b8438953370ef1c3490d72d
Kyiro <josereyesiriarte@gmail.com>
2025-02-23 23:59:54 -0400

    comentario en urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import PostList, PostDetail
-
+#Algo perron
 urlpatterns = [
     path('posts/', PostList.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
